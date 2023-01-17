dad2d66eccdbb2bb3c85a0caaef7aac9822748b2
Paul Holtfrerich <pholtfrerich@gmail.com>
2023-01-17 13:27:40 -0600

    First Commit

---
+++ Bitcoin_Helper.py
@@ -0,0 +1,145 @@
+from unittest import TestCase, TestSuite, TextTestRunner
+
+import hashlib
+
+
+# tag::source1[]
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+# end::source1[]
+
+
+def run(test):
+    suite = TestSuite()
+    suite.addTest(test)
+    TextTestRunner().run(suite)
+
+
+# tag::source4[]
+def hash160(s):
+    '''sha256 followed by ripemd160'''
+    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()  # <1>
+# end::source4[]
+
+
+def hash256(s):
+    '''two rounds of sha256'''
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
+"""encode_base58(s)
+    Description: converts a public key in bytes to an address in bytes
+    inputs:
+        s: a public key in SEC format
+    outputs:
+        An address to the public key in Base58 format
+"""
+def encode_base58(s):
+    count = 0
+    for c in s:  # <1>
+        if c == 0:
+            count += 1
+        else:
+            break
+    num = int.from_bytes(s, 'big')
+    prefix = '1' * count
+    result = ''
+    while num > 0:  # <2>
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+    return prefix + result  # <3>
+
+
+def encode_base58_checksum(b):
+    return encode_base58(b + hash256(b)[:4])
+# end::source3[]
+
+
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder='big')
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
+    return combined[1:-4]
+
+
+def little_endian_to_int(b):
+    '''little_endian_to_int takes byte sequence as a little-endian number.
+    Returns an integer'''
+    # use int.from_bytes()
+    
+    i = int.from_bytes(b, 'little')
+    return i
+    
+    raise NotImplementedError
+
+
+def int_to_little_endian(n, length):
+    '''endian_to_little_endian takes an integer and returns the little-endian
+    byte sequence of length'''
+    # use n.to_bytes()
+    
+    i = n.to_bytes(length, 'little')
+    return i
+    raise NotImplementedError
+
+
+"""read_varint(s):
+    inputs: stream, s
+    outputs: returns the integer encoded containing the number of inputs for the Tx
+"""
+def read_varint(s):
+    '''read_varint reads a variable integer from a stream'''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    else:
+        # anything else is just the integer
+        return i
+
+"""encode_varint(s): (does opposite of read(s))
+    inputs: integer for number of inputs in Tx
+    outputs: returns the varint byte representation for the number of inputs in Tx
+"""
+def encode_varint(i):
+    '''encodes an integer as a varint'''
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+# end::source1[]
+
+
+class HelperTest(TestCase):
+
+    def test_little_endian_to_int(self):
+        h = bytes.fromhex('99c3980000000000')
+        want = 10011545
+        self.assertEqual(little_endian_to_int(h), want)
+        h = bytes.fromhex('a135ef0100000000')
+        want = 32454049
+        self.assertEqual(little_endian_to_int(h), want)
+
+    def test_int_to_little_endian(self):
+        n = 1
+        want = b'\x01\x00\x00\x00'
+        self.assertEqual(int_to_little_endian(n, 4), want)
+        n = 10011545
+        want = b'\x99\xc3\x98\x00\x00\x00\x00\x00'
+        self.assertEqual(int_to_little_endian(n, 8), want)
